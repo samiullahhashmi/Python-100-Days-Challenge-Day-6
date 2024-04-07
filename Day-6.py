0b4b19e046fe85e4168471c1142dfdfc76f8173c
Samiullah Hashmi <samiullahhashmi27@gmail.com>
2024-04-07 17:26:47 +0500

    Day - 6

---
+++ Day-6.py
@@ -0,0 +1,21 @@
+# Day 6 Project : Done on https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+# put this code in the website : 
+
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+        
+#     elif front_is_clear():
+#         move()
+    
+#     else:
+#         turn_left()
+
